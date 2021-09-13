c89edd91f7ea73d2fc4ddda387767bf83830be19
BastienTest <73186901+BastienTest@users.noreply.github.com>
2021-09-13 11:50:48 +0200

    1. oppgave ferdig

--- src/UkeENOpp/Oppa.java
+++ src/UkeENOpp/Oppa.java
@@ -1,4 +1,22 @@
 package UkeENOpp;
 
 public class Oppa {
+    public static int min(int[] a) {
+        if (a.length < 1) {
+            throw new java.util.NoSuchElementException("tabelen er tom");
+        }
+        int indexMin = a.length - 1;
+        for (int i = a.length - 1; i >= 0; i--) {
+            if (a[i] < a[indexMin]) {
+                indexMin = i;
+            }
+        }
+        return indexMin;
+    }
+
+
+    public static void main(String[] args) {
+        int[] a = {1, 3, 6, 2, 9, 4, 12, 13};
+        System.out.println(min(a));
+    }
 }
